b45c7d381a394469f3688f90d83c217e2657973b
Filip Jakubinski <filip.jakubinski@gmail.com>
2025-03-27 14:58:04 +0100

    1 rep

--- manipulator.py
+++ manipulator.py
@@ -5,15 +5,15 @@ GPIO.setmode(GPIO.BCM)
 
 # --- PINY ---
 PWM_PIN = 18     # Sterowanie serwem (PWM)
-OUTPUT_PIN = 23  # Wyjście – domyślnie HIGH, przy przycisku LOW
-BUTTON_PIN = 14  # Przycisk
+#OUTPUT_PIN = 23  # Wyjście – domyślnie HIGH, przy przycisku LOW
+BUTTON_PIN = 23  # Przycisk
 
 # --- KONFIGURACJA GPIO ---
 GPIO.setup(PWM_PIN, GPIO.OUT)
-GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
+#GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+#GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
 
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PULL_UP)
 
 # --- INICJALIZACJA PWM (50Hz dla serwa) ---
 pwm = GPIO.PWM(PWM_PIN, 50)
@@ -26,26 +26,24 @@ def set_angle(angle):
     time.sleep(0.5)
     pwm.ChangeDutyCycle(0)
 
-position = 0
+#position = 0
 try:
     print("Czekam na wciśnięcie przycisku...")
 
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            GPIO.output(OUTPUT_PIN, GPIO.LOW)  # Wciśnięcie = niski stan
+            #GPIO.output(OUTPUT_PIN, GPIO.LOW)  # Wciśnięcie = niski stan
             print("Przycisk wciśnięty – napięcie LOW + ruch serwa")
-
-            if position == 0:
                 set_angle(180)
-                position = 1
+                #position = 1
                 time.sleep(1)
-            elif position == 1:
-                set_angle(0)
-                position = 0
-                time.sleep(1)
+
 
         else:
-            GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
+            #GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
+            set_angle(0)
+            #position = 0
+            time.sleep(1)
 
         time.sleep(0.1)
 
