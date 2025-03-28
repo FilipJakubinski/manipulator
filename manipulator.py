8615c6c7329c4cc63b0455174e6d450e9d4a2818
Filip Jakubinski <filip.jakubinski@gmail.com>
2025-03-28 13:01:43 +0100

    1 rep

--- manipulator.py
+++ manipulator.py
@@ -5,14 +5,10 @@ GPIO.setmode(GPIO.BCM)
 
 # --- PINY ---
 PWM_PIN = 18     # Sterowanie serwem (PWM)
-#OUTPUT_PIN = 23  # Wyjście – domyślnie HIGH, przy przycisku LOW
 BUTTON_PIN = 23  # Przycisk
 
 # --- KONFIGURACJA GPIO ---
 GPIO.setup(PWM_PIN, GPIO.OUT)
-#GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-#GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
-
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # --- INICJALIZACJA PWM (50Hz dla serwa) ---
@@ -23,26 +19,32 @@ pwm.start(0)
 def set_angle(angle):
     duty = 2 + (angle / 18)
     pwm.ChangeDutyCycle(duty)
-    time.sleep(0.5)
+    time.sleep(0.3)  # Krótszy czas oczekiwania
     pwm.ChangeDutyCycle(0)
 
-position = 0
+# --- ZMIENNA DO PRZECHOWYWANIA STANU ---
+position = 0  # 0 = zamknięte, 1 = otwarte
+
+# --- FUNKCJA OBSŁUGI PRZYCISKU ---
+def button_pressed(channel):
+    global position
+    if position == 0:
+        set_angle(180)
+        position = 1
+        print("Otwarte")
+    else:
+        set_angle(0)
+        position = 0
+        print("Zamknięte")
+
+# --- DETEKCJA PRZYCISKU ---
+GPIO.add_event_detect(BUTTON_PIN, GPIO.FALLING, callback=button_pressed, bouncetime=300)
+
+# --- GŁÓWNA PĘTLA ---
 try:
     print("Czekam na wciśnięcie przycisku...")
-
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            if position == 0:
-                set_angle(180)
-                position=1
-                GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-                print("otwarte")
-            else:
-                set_angle(0)
-                position=0
-                GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-                print("zamkniete")
-
+        time.sleep(1)  # Zmniejsza obciążenie procesora
 
 except KeyboardInterrupt:
     print("Zatrzymano program")
