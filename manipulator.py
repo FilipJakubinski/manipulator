fca877e91cfdae37bad36dcc3a7d5a1bddd680fc
Filip Jakubinski <filip.jakubinski@gmail.com>
2025-03-28 13:20:27 +0100

    1 rep

--- manipulator.py
+++ manipulator.py
@@ -4,39 +4,43 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # --- PINY ---
-PWM_PIN = 18     # Sterowanie serwem (PWM)
-#OUTPUT_PIN = 23  # Wyjście – domyślnie HIGH, przy przycisku LOW
+PWM_PIN = 18  # Sterowanie serwem (PWM)
 BUTTON_PIN = 23  # Przycisk
 
 # --- KONFIGURACJA GPIO ---
 GPIO.setup(PWM_PIN, GPIO.OUT)
-#GPIO.setup(OUTPUT_PIN, GPIO.OUT)
-#GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Domyślnie HIGH
-
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # --- INICJALIZACJA PWM (50Hz dla serwa) ---
 pwm = GPIO.PWM(PWM_PIN, 50)
 pwm.start(0)
 
+
 # --- FUNKCJA USTAWIANIA KĄTA ---
 def set_angle(angle):
     duty = 2 + (angle / 18)
     pwm.ChangeDutyCycle(duty)
-    time.sleep(0.5)
-    pwm.ChangeDutyCycle(0)
+    time.sleep(0.5)  # Czekaj, aż serwo się ustawi
+    pwm.ChangeDutyCycle(0)  # Zatrzymanie PWM, aby uniknąć drgań
+
+
+# --- USTAWIAMY SERWO NA ZAMKNIĘTE ---
+set_angle(0)
+print("Serwo ustawione na zamknięte")
 
 try:
     print("Czekam na wciśnięcie przycisku...")
 
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            set_angle(180)
-            GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-            print("otwarte")
-        else:
-            set_angle(0)
-            print("zamykam")
+        if GPIO.input(BUTTON_PIN) == GPIO.LOW:  # Wciśnięty przycisk
+            set_angle(180)  # Otwórz serwo
+            print("Otwarte")
+
+            while GPIO.input(BUTTON_PIN) == GPIO.LOW:
+                time.sleep(0.1)  # Czekaj, aż użytkownik puści przycisk
+
+            print("Zamykam...")
+            set_angle(0)  # Zamknij serwo
 
 except KeyboardInterrupt:
     print("Zatrzymano program")
