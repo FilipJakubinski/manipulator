c0fad997e92bdb9e7e46b7153076a40edd3d2810
Filip Jakubinski <filip.jakubinski@gmail.com>
2025-03-28 13:33:07 +0100

    1 rep

--- manipulator.py
+++ manipulator.py
@@ -13,7 +13,7 @@ GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # --- INICJALIZACJA PWM (50Hz dla serwa) ---
 pwm = GPIO.PWM(PWM_PIN, 50)
-pwm.start(2.5)
+pwm.start(0)
 
 
 # --- FUNKCJA USTAWIANIA KĄTA ---
